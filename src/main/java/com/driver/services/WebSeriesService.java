51a1a257784fc912f98ca0acaeabefc9b2764585
kamal <tekkensingle@gmail.com>
2024-02-23 13:32:29 +0530

    4th

--- src/main/java/com/driver/services/WebSeriesService.java
+++ src/main/java/com/driver/services/WebSeriesService.java
@@ -21,31 +21,29 @@ public class WebSeriesService {
 
     public Integer addWebSeries(WebSeriesEntryDto webSeriesEntryDto)throws  Exception{
 
+
+
+            //Add a webSeries to the database and update the ratings of the productionHouse
+            //Incase the seriesName is already present in the Db throw Exception("Series is already present")
+            //use function written in Repository Layer for the same
+            //Dont forget to save the production and webseries Repo
+            WebSeries webSeriesCheck=webSeriesRepository.findBySeriesName(webSeriesEntryDto.getSeriesName());
+            if(webSeriesCheck!=null)
+            {
+                throw new Exception("Series is already present");
+            }
+            WebSeries webSeries=new WebSeries(webSeriesEntryDto.getSeriesName(),webSeriesEntryDto.getAgeLimit(),
+                    webSeriesEntryDto.getRating(),webSeriesEntryDto.getSubscriptionType());
             ProductionHouse productionHouse=productionHouseRepository.findById(webSeriesEntryDto.getProductionHouseId()).get();
-            String seriesName= webSeriesEntryDto.getSeriesName();
-            WebSeries findwebSeries=webSeriesRepository.findBySeriesName(seriesName);
-
-        if( findwebSeries!=null){
-            throw new Exception("Series is already present");
-        }
-           WebSeries webSeries=new WebSeries(webSeriesEntryDto.getSeriesName(), webSeriesEntryDto.getAgeLimit(),webSeriesEntryDto.getRating(),webSeriesEntryDto.getSubscriptionType());
-
-            double productionRating=(productionHouse.getRatings())*(productionHouse.getWebSeriesList().size());
-            double newRating=(productionRating+webSeriesEntryDto.getRating())/(productionHouse.getWebSeriesList().size()+1);
-
+            double sumOfRating=(productionHouse.getRatings()*productionHouse.getWebSeriesList().size());
+            double newRating=((sumOfRating+webSeries.getRating())/(productionHouse.getWebSeriesList().size()+1));
             productionHouse.setRatings(newRating);
-            productionHouse.getWebSeriesList().add(webSeries);
             webSeries.setProductionHouse(productionHouse);
-
-           webSeries= webSeriesRepository.save(webSeries);
-           productionHouseRepository.save(productionHouse);
-
-        //Add a webSeries to the database and update the ratings of the productionHouse
-        //Incase the seriesName is already present in the Db throw Exception("Series is already present")
-        //use function written in Repository Layer for the same
-        //Dont forget to save the production and webseries Repo
-
-        return webSeries.getId();
+            productionHouse.getWebSeriesList().add(webSeries);
+            WebSeries webSeries1 = webSeriesRepository.save(webSeries);
+            productionHouseRepository.save(productionHouse);
+            return webSeries1.getId();
+        }
     }
 
-}
+
